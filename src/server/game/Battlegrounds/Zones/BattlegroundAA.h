15822b2d460b62f6faf0a7d0cc13a308fed080c3
Bootz <bootz@projectskyfire.org>
2012-02-20 17:57:08 -0600

    reimporting Battlegrounds, these are now correctly renamed.
    Signed-off-by: Bootz <bootz@projectskyfire.org>

---
+++ src/server/game/Battlegrounds/Zones/BattlegroundAA.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2010-2012 Oregon <http://www.oregoncore.com/>
+ * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __BATTLEGROUNDAA_H
+#define __BATTLEGROUNDAA_H
+
+class BattleGround;
+
+class BattleGroundAAScore : public BattleGroundScore
+{
+    public:
+        BattleGroundAAScore() {};
+        virtual ~BattleGroundAAScore() {};
+        // TODO fix me
+};
+
+class BattleGroundAA : public BattleGround
+{
+    friend class BattleGroundMgr;
+
+    public:
+        BattleGroundAA();
+        ~BattleGroundAA();
+        void Update(time_t diff);
+
+        /* inherited from BattlegroundClass */
+        virtual void AddPlayer(Player *plr);
+        virtual void StartingEventCloseDoors();
+        virtual void StartingEventOpenDoors();
+
+        void RemovePlayer(Player *plr, uint64 guid);
+        void HandleAreaTrigger(Player *Source, uint32 Trigger);
+        bool SetupBattleGround();
+        void HandleKillPlayer(Player* player, Player *killer);
+};
+#endif
+
