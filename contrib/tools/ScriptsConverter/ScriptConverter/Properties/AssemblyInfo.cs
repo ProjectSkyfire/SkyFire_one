b9225ce1cd0daff682811ee734e8b1363faebd71
bootz <bootz@projectskyfire.org>
2012-06-22 14:18:33 -0500

    Contrib/Tools: Updated ScriptConverter.
    still needs some little refinements...
    
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- contrib/tools/ScriptsConverter/ScriptConverter/Properties/AssemblyInfo.cs
+++ contrib/tools/ScriptsConverter/ScriptConverter/Properties/AssemblyInfo.cs
@@ -1,25 +1,31 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region
+
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("ScriptConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ScriptConverter")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("39b64a0a-a460-4177-925f-b9bdda4f2a1d")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +38,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
