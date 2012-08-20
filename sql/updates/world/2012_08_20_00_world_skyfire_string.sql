bd5d4a63fe07e4484503325dc1b0cec4359a5354
bootz <bootz@projectskyfire.org>
2012-08-20 08:12:09 -0500

    DB: Added missing string for autobroadcast. forum fix thanks to: Zevran for catching it.
    Signed-off-by: bootz <bootz@projectskyfire.org>

---
+++ sql/updates/world/2012_08_20_00_world_skyfire_string.sql
@@ -0,0 +1,1 @@
+INSERT  INTO `skyfire_string`(`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`) VALUES (11000,'|cffffff00[|c00077766Autobroadcast|cffffff00]: |cFFF222FF%s|r',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
