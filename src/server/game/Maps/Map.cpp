bf89245d168808e30baccedfea63decddcf836a3
KillaVulkan <KillaVulkan@hotmail.de>
2012-09-09 11:07:54 -0700

    Trinity -> SkyFire and TRINITY -> SKYFIRE PART 5 The last Patch? :D

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2010-2012 Oregon <http://www.oregoncore.com/>
- * Copyright (C) 2008-2012 SkyFireCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
