2169a3bce7ea3be46cb4a7d1512b527930242036
bootz <bootz@projectskyfire.org>
2012-09-27 12:25:52 -0500

    Core/Contrib: adjustments done in ScriptConverter. - this tool will be used in the future to convert all the mangos - based scripts over to TSCR.
    Signed-off-by: bootz <bootz@projectskyfire.org>

--- contrib/tools/ScriptsConverter/ScriptConverter/Program.cs
+++ contrib/tools/ScriptsConverter/ScriptConverter/Program.cs
@@ -1,231 +1,219 @@
-﻿#region
-
-using System;
+﻿using System;
 using System.Collections;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.IO;
 
-#endregion
-
-namespace ScriptConverter
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
+namespace ScriptConverter {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length != 1) {
                 Console.WriteLine("Usage: ScriptsConverter.exe [path_to_dir|path_to_file]");
-            }
-            else
-            {
+            } else {
                 string path = args[0];
-                if (File.Exists(path))
-                {
+                if (File.Exists(path)) {
                     ProcessFile(path);
-                }
-                else if (!Directory.Exists(path))
-                {
-                    Console.WriteLine(
-                        "Invalid file or directory specified.\r\n\r\nUsage: ScriptsConverter.exe [path_to_dir|path_to_file]");
-                }
-                else
-                {
+                } else if (Directory.Exists(path)) {
                     ProcessDirectory(path);
+                } else {
+                    Console.WriteLine("Invalid file or directory specified.\r\n\r\nUsage: ScriptsConverter.exe [path_to_dir|path_to_file]");
                 }
             }
         }
 
-        private static void ProcessDirectory(string path)
-        {
-            String[] files = Directory.GetFiles(path, "*.cpp");
-            foreach (String file in files)
-            {
+        static void ProcessDirectory(string path) {
+            string[] files = Directory.GetFiles(path, "*.cpp");
+            foreach (string file in files) {
                 ProcessFile(file);
             }
-            String[] dirs = Directory.GetDirectories(path);
-            foreach (String dir in dirs)
-            {
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs) {
                 ProcessDirectory(dir);
             }
         }
 
-        private static string GetMethod(string method, ref string txt, ref int minPos)
-        {
-            Match m = new Regex(method + "(\\s|:|[(])").Match(txt);
-            var pos = m.Index;
-            Int32 lastPos = txt.IndexOf("\n}", pos, StringComparison.Ordinal);
-            if (m.Success)
-            {
-                while (pos-- >= 0 && pos < txt.Length)
+        class ScriptData {
+            public int type = 0;
+            public string name;
+            public ArrayList methods = new ArrayList();
+            public string instanceName = null;
+            public string aiName = null;
+            public string[] special = new string[] { "GetAI_", "GetInstance_", "GetInstanceData_" };
+
+            public void AddMethod(string method) {
+                methods.Add(method);
+                int i = 0;
+                foreach (string s in special) {
+                    ++i;
+                    int pos = method.IndexOf(s);
+                    if (pos != -1) {
+                        type = i;
+                        string name = method.Substring(pos + s.Length);
+                        if (i == 1) {
+                            aiName = name + "AI";
+                        }
+                        if (i == 2 || i == 3)
+                            instanceName = name;
+                    }
+                }
+            }
+
+            public override string ToString() {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("Script: {0}\n", name);
+                foreach (string method in methods)
+                    sb.Append("    ").Append(method).Append("\n");
+                sb.Append("\n");
+                return sb.ToString();
+            }
+        }
+
+        static string GetMethod(string method, ref string txt, ref int minPos) {
+            string res = null;
+            Regex r = new Regex(method + "(\\s|:|[(])");
+            Match m = r.Match(txt);
+            if (m.Success) {
+                int pos = m.Index;
+                while (pos-- >= 0 && pos < txt.Length) {
                     if (txt[pos] == '\n') break;
+                }
                 //pos++;
-                if (lastPos != -1)
-                {
+                int lastPos = txt.IndexOf("\n}", pos);
+                if (lastPos != -1) {
                     lastPos += 2;
-                    while (lastPos++ >= 0 && lastPos < txt.Length)
-                    {
+                    while (lastPos++ >= 0 && lastPos < txt.Length) {
                         if (txt[lastPos] == '\n') break;
                     }
-                    return txt.Substring(pos, lastPos - pos);
-/*
+                    res = txt.Substring(pos, lastPos - pos);
                     txt = txt.Remove(pos, lastPos - pos);
-*/
                 }
                 if (pos < minPos)
                     minPos = pos;
             }
-            return null;
+            return res;
         }
 
-        private static void ProcessFile(string filePath)
-        {
+        static void ProcessFile(string filePath) {
             Console.WriteLine(filePath);
 
             string txt = File.ReadAllText(filePath);
             string[] lines = File.ReadAllLines(filePath);
             Array.Reverse(lines);
 
+            ArrayList scripts = new ArrayList();
             ScriptData data = null;
             bool scriptStart = false;
-            var scripts = new ArrayList();
-            foreach (string line in lines)
-            {
-                if (line.IndexOf("Script *", StringComparison.Ordinal) != -1)
-                {
+            foreach (string line in lines) {
+                if (line.IndexOf("Script *") != -1) {
                     break;
                 }
-                if (line.IndexOf("->RegisterSelf();", StringComparison.Ordinal) != -1)
-                {
+                if (line.IndexOf("->RegisterSelf();") != -1) {
                     scriptStart = true;
                     data = new ScriptData();
                     continue;
                 }
-                if (scriptStart)
-                {
-                    if (line.IndexOf("= new Script", StringComparison.Ordinal) != -1)
-                    {
+                if (scriptStart) {
+                    if (line.IndexOf("= new Script") != -1) {
                         scriptStart = false;
                         scripts.Add(data);
                         data = null;
                         continue;
                     }
-                    var r = new Regex("newscript->([a-zA-Z]+) *= *&?([\"_a-zA-Z0-9]+);");
+                    Regex r = new Regex("newscript->([a-zA-Z]+) *= *&?([\"_a-zA-Z0-9]+);");
                     Match m = r.Match(line);
-                    if (m.Success)
-                    {
-                        if (m.Groups[1].Value.Equals("Name"))
-                        {
-                            data.Name = m.Groups[2].Value.Trim(new[] {'"'});
-                        }
-                        else
-                        {
+                    if (m.Success) {
+                        if (m.Groups[1].Value.Equals("Name")) {
+                            data.name = m.Groups[2].Value.Trim(new char[] { '"' });
+                        } else {
                             data.AddMethod(m.Groups[2].Value);
                         }
                     }
+                    continue;
                 }
             }
-            if (scripts.Count != 0)
-            {
+            if (scripts.Count != 0) {
                 string register = "";
-                foreach (ScriptData sd in scripts)
-                {
+                foreach (ScriptData sd in scripts) {
+                    string ss = "";
                     Console.WriteLine(sd);
                     int minPos = txt.Length;
-                    String ss =
-                        (from string method in sd.Methods select GetMethod(method, ref txt, ref minPos)).Aggregate("",
-                                                                                                                   (
-                                                                                                                       current,
-                                                                                                                       s)
-                                                                                                                   =>
-                                                                                                                   current +
-                                                                                                                   (s +
-                                                                                                                    "\n"));
-                    if (sd.InstanceName != null)
-                    {
-                        String s = GetMethod("struct " + sd.InstanceName, ref txt, ref minPos);
+                    foreach (string method in sd.methods) {
+                        string s = GetMethod(method, ref txt, ref minPos);
                         ss += s + "\n";
                     }
-                    if (sd.AiName != null)
-                    {
-                        string ai = GetMethod("struct " + sd.AiName, ref txt, ref minPos);
-                        if (ai != null)
-                        {
-                            var r = new Regex("\\S+ " + sd.AiName + "::([^( ]+)");
-                            while (r.IsMatch(txt))
-                            {
+                    if (sd.instanceName != null) {
+                        string s = GetMethod("struct " + sd.instanceName, ref txt, ref minPos);
+                        ss += s + "\n";
+                    }
+                    if (sd.aiName != null) {
+                        string ai = GetMethod("struct " + sd.aiName, ref txt, ref minPos);
+                        if (ai != null) {
+                            string sm = null;
+                            Regex r = new Regex("\\S+ " + sd.aiName + "::([^( ]+)");
+                            while (r.IsMatch(txt)) {
                                 Match m = r.Match(txt);
                                 int startPos = m.Index;
-                                int endPos = txt.IndexOf("\n}", startPos, StringComparison.Ordinal);
+                                int endPos = txt.IndexOf("\n}", startPos);
                                 if (endPos != -1)
                                     endPos += 2;
-                                while (endPos++ >= 0 && endPos < txt.Length)
-                                {
+                                while (endPos++ >= 0 && endPos < txt.Length) {
                                     if (txt[endPos] == '\n') break;
                                 }
-                                string sm = txt.Substring(startPos, endPos - startPos);
+                                sm = txt.Substring(startPos, endPos - startPos);
                                 txt = txt.Remove(startPos, endPos - startPos);
-                                {
+                                if (sm != null) {
                                     sm = sm.Replace("\n", "\n    ");
-                                    var r1 = new Regex("\\S+ " + m.Groups[1] + " *\\([^)]*\\) *;");
+                                    Regex r1 = new Regex("\\S+ " + m.Groups[1] + " *\\([^)]*\\) *;");
                                     Match m1 = r1.Match(ai);
-                                    if (m1.Success)
-                                    {
+                                    if (m1.Success) {
                                         ai = r1.Replace(ai, sm);
                                     }
                                 }
                             }
-                            ai = ai.Replace(sd.AiName + "::", "");
+                            ai = ai.Replace(sd.aiName + "::", "");
                             ss += ai + "\n";
                         }
                     }
-                    if (ss.Length != 0)
-                    {
+                    if (ss.Length != 0) {
                         string typeName = "UnknownScript";
-                        switch (sd.Type)
-                        {
-                            case 1:
-                                typeName = "CreatureScript";
-                                break;
-                            case 2:
-                                typeName = "InstanceMapScript";
-                                break;
+                        switch (sd.type) {
+                            case 1: typeName = "CreatureScript"; break;
+                            case 2: typeName = "InstanceMapScript"; break;
                             default:
-                                if (sd.Name.IndexOf("npc", StringComparison.Ordinal) == 0)
+                                if (sd.name.IndexOf("npc") == 0)
                                     typeName = "CreatureScript";
-                                else if (sd.Name.IndexOf("mob", StringComparison.Ordinal) == 0)
+                                else if (sd.name.IndexOf("mob") == 0)
                                     typeName = "CreatureScript";
-                                else if (sd.Name.IndexOf("boss_", StringComparison.Ordinal) == 0)
+                                else if (sd.name.IndexOf("boss_") == 0)
                                     typeName = "CreatureScript";
-                                else if (sd.Name.IndexOf("item_", StringComparison.Ordinal) == 0)
+                                else if (sd.name.IndexOf("item_") == 0)
                                     typeName = "ItemScript";
-                                else if (sd.Name.IndexOf("go_", StringComparison.Ordinal) == 0)
+                                else if (sd.name.IndexOf("go_") == 0)
                                     typeName = "GameObjectScript";
-                                else if (sd.Name.IndexOf("at_", StringComparison.Ordinal) == 0)
+                                else if (sd.name.IndexOf("at_") == 0)
                                     typeName = "AreaTriggerScript";
-                                else if (sd.Name.IndexOf("instance_", StringComparison.Ordinal) == 0)
+                                else if (sd.name.IndexOf("instance_") == 0)
                                     typeName = "InstanceMapScript";
                                 break;
                         }
-                        if (sd.InstanceName != null)
-                            ss = ss.Replace(sd.InstanceName, sd.InstanceName + "_InstanceMapScript");
+                        if (sd.instanceName != null)
+                            ss = ss.Replace(sd.instanceName, sd.instanceName + "_InstanceMapScript");
                         ss = ss.Replace("\n", "\n    ");
-                        ss = "class " + sd.Name + " : public " + typeName + "\n{\npublic:\n    " +
-                             sd.Name + "() : " + typeName + "(\"" + sd.Name + "\") { }\n" + ss + "\n};";
-                        ss = ss.Replace("_" + sd.Name, "");
+                        ss = "class " + sd.name + " : public " + typeName + "\n{\npublic:\n    " +
+                            sd.name + "() : " + typeName + "(\"" + sd.name + "\") { }\n" + ss + "\n};";
+                        ss = ss.Replace("_" + sd.name, "");
                         ss = ss.Replace("AIAI", "AI");
                         ss = ss.Replace("    \r\n", "\r\n");
                         ss = ss.Replace("    \n", "\n");
                         txt = txt.Insert(minPos, ss);
-                        register = "    new " + sd.Name + "();\n" + register;
+                        register = "    new " + sd.name + "();\n" + register;
                     }
                 }
-                var r2 = new Regex("void +AddSC_([_a-zA-Z0-9]+)");
+                Regex r2 = new Regex("void +AddSC_([_a-zA-Z0-9]+)");
                 Match m2 = r2.Match(txt);
-                if (m2.Success)
-                {
+                if (m2.Success) {
                     txt = txt.Remove(m2.Index);
                     txt += "void AddSC_" + m2.Groups[1].Value + "()\n{\n" + register + "}\n";
                 }
@@ -234,51 +222,5 @@ namespace ScriptConverter
                 File.WriteAllText(filePath, txt);
             }
         }
-
-        #region Nested type: ScriptData
-
-        private class ScriptData
-        {
-            public readonly ArrayList Methods = new ArrayList();
-            private readonly string[] _special = new[] {"GetAI_", "GetInstance_", "GetInstanceData_"};
-            public string AiName;
-            public string InstanceName;
-            public string Name;
-            public int Type;
-
-            public void AddMethod(string method)
-            {
-                Methods.Add(method);
-                int i = 0;
-                foreach (string s in _special)
-                {
-                    ++i;
-                    int pos = method.IndexOf(s, StringComparison.Ordinal);
-                    string name = method.Substring(pos + s.Length);
-                    if (pos != -1)
-                    {
-                        Type = i;
-                        if (i == 1)
-                        {
-                            AiName = name + "AI";
-                        }
-                        if (i == 2 || i == 3)
-                            InstanceName = name;
-                    }
-                }
-            }
-
-            public override string ToString()
-            {
-                var sb = new StringBuilder();
-                sb.AppendFormat("Script: {0}\n", Name);
-                foreach (string method in Methods)
-                    sb.Append("    ").Append(method).Append("\n");
-                sb.Append("\n");
-                return sb.ToString();
-            }
-        }
-
-        #endregion
     }
 }
