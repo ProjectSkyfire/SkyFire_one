815c7f8f0c9f91a2139bfab6cebda940545bced8
Bootz <bootz@projectskyfire.org>
2017-06-30 10:04:26 -0500

    Build/CMake: Fixed Typo for Visual Studio builds.
    NOTE - this is a strict build type.
    so everyone is on the same page with their building tools,
    So now we all know what version of MSVS everyone is using when asking for assistance, less frustration on our side,
    and better help to those that would need it.
    
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -7,9 +7,9 @@ set(ACE_LIBRARY "ace")
 set(BZIP2_LIBRARIES "bzip2")
 set(ZLIB_LIBRARIES "zlib")
 
-# We require at least Visual Studio 2015 (aka 14.0) which has version nr 1900.
+# We require at least Visual Studio 15 2017(aka 14.0) which has version nr 1900.
 IF(NOT FORCE_UNSUPPORTED_COMPILER AND MSVC_VERSION LESS 1900)
-  MESSAGE(FATAL_ERROR "Visual Studio 2015 or newer is required!")
+  MESSAGE(FATAL_ERROR "Visual Studio 15 2017 or newer is required!")
 ENDIF()
 
 # check the CMake preload parameters (commented out by default)
