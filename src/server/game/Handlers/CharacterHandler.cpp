b69c2e0a24af0f8c8f17422a4ec679455b43a556
Bootz <bootz@projectskyfire.org>
2017-07-24 16:38:22 -0500

    Clean-up from last commit: Tabs2Spaces.
    (cherry picked from commit 65f2ebc9315f11fb0d0352ce4a32823a3f71c51f)

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -78,7 +78,7 @@ bool LoginQueryHolder::Initialize()
         "yesterdayHonorPoints, totalKills, todayKills, yesterdayKills, "
         "chosenTitle, watchedFaction, drunk, health, "
         "powerMana, powerRage, powerFocus, powerEnergy, powerHappiness, instance_id, "
-		"exploredZones, equipmentCache,ammoId, knownTitles FROM characters WHERE guid = '%u'", GUID_LOPART(m_guid));
+        "exploredZones, equipmentCache,ammoId, knownTitles FROM characters WHERE guid = '%u'", GUID_LOPART(m_guid));
     res &= SetPQuery(PLAYER_LOGIN_QUERY_LOADGROUP,           "SELECT leaderGuid FROM group_member WHERE memberGuid ='%u'", GUID_LOPART(m_guid));
     res &= SetPQuery(PLAYER_LOGIN_QUERY_LOADBOUNDINSTANCES,  "SELECT id, permanent, map, difficulty, resettime FROM character_instance LEFT JOIN instance ON instance = id WHERE guid = '%u'", GUID_LOPART(m_guid));
     res &= SetPQuery(PLAYER_LOGIN_QUERY_LOADAURAS,           "SELECT caster_guid, spell, effect_index, stackcount, amount, maxduration, remaintime, remaincharges FROM character_aura WHERE guid = '%u'", GUID_LOPART(m_guid));
