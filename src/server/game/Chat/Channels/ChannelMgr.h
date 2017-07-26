910372d950689a821b7178c5ae3280b6c9d72c5a
Bootz <bootz@projectskyfire.org>
2017-07-26 11:05:04 -0500

    Repo/Headers: Updated copyrights and GNU versioning, Updated ReadMe.
    Signed-off-by: Bootz <bootz@projectskyfire.org>
    (cherry picked from commit fb3979418bfd6da22a9b2780c4099d9e8584ef4d)

--- src/server/game/Chat/Channels/ChannelMgr.h
+++ src/server/game/Chat/Channels/ChannelMgr.h
@@ -1,7 +1,8 @@
 /*
- * Copyright (C) 2010-2012 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2012 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2011-2017 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2010-2017 Oregon <http://www.oregoncore.com/>
+ * Copyright (C) 2005-2017 MaNGOS <https://www.getmangos.eu/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
