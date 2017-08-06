7719189acca3db93724542334729a7b9581d8ddc
Bootz <bootz@projectskyfire.org>
2017-08-06 17:29:11 -0500

    Added (WorldScript) NPC_Taxi into build to find the linking error with Scripts.
    Note: Only World Scripts have been converted. All give same link error for each script. just needed one to sort the issue out.
    
    Signed-off-by: Bootz <bootz@projectskyfire.org>
    (cherry picked from commit bc79d9ca3fbeb626ebc28ded2347e7bc78dd6bdc)

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -42,11 +42,11 @@ void AddSC_go_scripts();
 void AddSC_guards();
 void AddSC_item_scripts();
 //void AddSC_npc_professions();
-void AddSC_npc_innkeeper();
-void AddSC_npcs_special();
+//void AddSC_npc_innkeeper();
+//void AddSC_npcs_special();
 void AddSC_npc_taxi();
-void AddSC_achievement_scripts();
-void AddSC_npc_mount();                      // npc mount
+//void AddSC_achievement_scripts();
+//void AddSC_npc_mount();                      // npc mount
 
 //eastern kingdoms
 void AddSC_alterac_valley();                 //Alterac Valley
@@ -465,12 +465,11 @@ void AddWorldScripts()
     AddSC_guards();
     AddSC_item_scripts();
     //AddSC_npc_professions();
-    AddSC_npc_innkeeper();
-    AddSC_npcs_special();
+    //AddSC_npc_innkeeper();
+    //AddSC_npcs_special();
     AddSC_npc_taxi();
-    AddSC_achievement_scripts();
-    AddSC_npc_mount();                      // npc mount
-    AddSC_chat_log();
+    //AddSC_npc_mount();                      // npc mount
+    //AddSC_chat_log();
 #endif
 }
 
