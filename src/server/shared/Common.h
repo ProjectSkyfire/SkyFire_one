6c334756e9bfb2f6bd647d895b26d03535749601
Bootz <bootz@projectskyfire.org>
2017-09-17 14:49:07 -0500

    Removed warning-ignores from Common.h.  - No more "in-core silencing", resides with the compiler build-time rules.
    Signed-off-by: Bootz <bootz@projectskyfire.org>
    (cherry picked from commit 21ca4ad77ba186cf3692cd0a556ea3b5ce79a97a)

--- src/server/shared/Common.h
+++ src/server/shared/Common.h
@@ -60,22 +60,6 @@
 #endif //HAVE_CONFIG_H
 
 #include "Define.h"
-
-#if COMPILER == COMPILER_MICROSOFT
-#   pragma warning(disable:4996)                            // 'function': was declared deprecated
-#ifndef __SHOW_STUPID_WARNINGS__
-#   pragma warning(disable:4005)                            // 'identifier' : macro redefinition
-#   pragma warning(disable:4018)                            // 'expression' : signed/unsigned mismatch
-#   pragma warning(disable:4244)                            // 'argument' : conversion from 'type1' to 'type2', possible loss of data
-#   pragma warning(disable:4267)                            // 'var' : conversion from 'size_t' to 'type', possible loss of data
-#   pragma warning(disable:4305)                            // 'identifier' : truncation from 'type1' to 'type2'
-#   pragma warning(disable:4311)                            // 'variable' : pointer truncation from 'type' to 'type'
-#   pragma warning(disable:4355)                            // 'this' : used in base member initializer list
-#   pragma warning(disable:4800)                            // 'type' : forcing value to bool 'true' or 'false' (performance warning)
-#   pragma warning(disable:4522)                            // warning when class has 2 constructors
-#endif                                                      // __SHOW_STUPID_WARNINGS__
-#endif                                                      // __GNUC__
-
 #include "Dynamic/UnorderedMap.h"
 #include <stdio.h>
 #include <stdlib.h>
