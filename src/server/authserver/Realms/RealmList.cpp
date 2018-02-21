ea18b473bdd464991668ee6c11143027295535bc
Bootz <bootz@projectskyfire.org>
2018-02-21 05:50:42 -0600

    core/misc:
     - Fixed realm flags in realmlist
     - Use PLAYER_BYTES_3 instead of gender to avoid some of the displayid problems
       *This fix is for corpse turning into a cube,, still more to repair.
    
    Signed-off-by: Bootz <bootz@projectskyfire.org>
    (cherry picked from commit 8668b0eb8d542dbff528a57d84f0a79feb4ee457)

--- src/server/authserver/Realms/RealmList.cpp
+++ src/server/authserver/Realms/RealmList.cpp
@@ -72,7 +72,7 @@ void RealmList::UpdateRealms(bool init)
 {
     sLog->outDetail("Updating Realm List...");
 
-    QueryResult_AutoPtr result = LoginDatabase.Query("SELECT id, name, address, port, icon, flag, timezone, allowedSecurityLevel, population, gamebuild FROM realmlist WHERE flag 3 ORDER BY name");
+    QueryResult_AutoPtr result = LoginDatabase.Query("SELECT id, name, address, port, icon, flag, timezone, allowedSecurityLevel, population, gamebuild FROM realmlist WHERE flag <> 3 ORDER BY name");
 
     // Circle through results and add them to the realm map 
     if (result)
